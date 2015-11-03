b5ef7781dad0990649b3eb26c230e2e3fc39c863
Tammy <misstamar@gmail.com>
2015-11-02 20:35:16 -0800

    commit to push

--- level1.rb
+++ level1.rb
@@ -1,14 +1,6 @@
 
 class Player
   def play_turn(warrior)
-    if  warrior.feel.empty?
-      if warrior.health <= 6
-        warrior.rest!
-      else
-        warrior.walk!
-      end
-   else
-     warrior.attack!
-    end
+    warrior.walk!
   end
 end
